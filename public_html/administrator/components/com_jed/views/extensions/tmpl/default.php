da52958cd51522fb289568d9e917917f94a0935b
Roland Dalmulder <contact@rolandd.com>
2019-08-03 19:05:49 +0200

    Work on the listing view WIP #365
    Signed-off-by: Roland Dalmulder <contact@rolandd.com>

--- public_html/administrator/components/com_jed/views/extensions/tmpl/default.php
+++ public_html/administrator/components/com_jed/views/extensions/tmpl/default.php
@@ -6,69 +6,120 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
-
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Router\Route;
 
-$user       = Factory::getUser();
-$userId     = $user->get('id');
-$listOrder  = $this->escape($this->state->get('list.ordering'));
-$listDirn   = $this->escape($this->state->get('list.direction'));
-$canOrder   = $user->authorise('core.edit.state', 'com_jed.extensions');
-$saveOrder  = $listOrder == 't.ordering';
-$sortFields = $this->getSortFields();
+defined('_JEXEC') or die;
 
-HTMLHelper::_('formbehavior.chosen', 'select');
+/** @var JedViewExtensions $this */
+
+$user      = Factory::getUser();
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$userId    = $user->get('id');
 ?>
-<script type="text/javascript">
-    Joomla.orderTable = function () {
-        table = document.getElementById("sortTable");
-        direction = document.getElementById("directionTable");
-        order = table.options[table.selectedIndex].value;
-        if (order != '<?php echo $listOrder; ?>') {
-            dirn = 'asc';
-        } else {
-            dirn = direction.options[direction.selectedIndex].value;
-        }
-        Joomla.tableOrdering(order, dirn, '');
-    }
-</script>
-
-<form action="<?php echo Route::_('index.php?option=com_jed&view=extensions'); ?>" method="post" name="adminForm"
-      id="adminForm">
-	<?php if (!empty($this->sidebar)) : ?>
-	<div id="j-sidebar-container" class="span2">
+<form name="adminForm" id="adminForm" method="post" action="index.php?option=com_jed&view=extensions">
+    <div id="j-sidebar-container" class="span2">
 		<?php echo $this->sidebar; ?>
-	</div>
-	<div id="j-main-container" class="span10">
-		<?php else : ?>
-		<div id="j-main-container">
-			<?php endif; ?>
-			<?php
-			// Search tools bar
-			echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this));
-			?>
-			<div class="clearfix"></div>
-			<?php if (empty($this->items)) : ?>
-				<div class="alert alert-no-items">
-					<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
-				</div>
-			<?php else : ?>
-				<table class="table table-striped">
-					<thead><?php echo $this->loadTemplate('head'); ?></thead>
-					<tfoot><?php echo $this->loadTemplate('foot'); ?></tfoot>
-					<tbody><?php echo $this->loadTemplate('body'); ?></tbody>
-				</table>
-			<?php endif; ?>
+    </div>
+    <div id="j-main-container" class="span10">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th width="20">
+                    <input type="checkbox" name="toggle" value=""
+                           onclick="checkAll(<?php echo count($this->items); ?>);"/>
+                </th>
+                <th>
+                    &nbsp;
+                </th>
+                <th>
+					<?php echo Text::_('COM_JED_EXTENSIONS_TITLE'); ?>
+                </th>
+                <th>
+					<?php echo Text::_('COM_JED_EXTENSIONS_CATEGORY'); ?>
+                </th>
+                <th>
+					<?php echo Text::_('COM_JED_EXTENSIONS_APPROVED'); ?>
+                </th>
+                <th>
+					<?php echo Text::_('COM_JED_EXTENSIONS_DEVELOPER'); ?>
+                </th>
+                <th>
+					<?php echo Text::_('COM_JED_EXTENSIONS_TYPE'); ?>
+                </th>
+                <th>
+					<?php echo Text::_('COM_JED_EXTENSIONS_REVIEWCOUNT'); ?>
+                </th>
 
-			<input type="hidden" name="option" value="com_jed"/>
-			<input type="hidden" name="task" value=""/>
-			<input type="hidden" name="boxchecked" value="0"/>
-			<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
-			<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
-			<?php echo HTMLHelper::_('form.token'); ?>
+                <th width="5">
+					<?php echo Text::_('JGRID_HEADING_ID'); ?>
+                </th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <td colspan="3"><?php echo $this->pagination->getListFooter(); ?></td>
+            </tr>
+            </tfoot>
+            <tbody>
+			<?php foreach ($this->items as $i => $item):
+
+				$ordering = ($listOrder === 't.id');
+				$canCreate = $user->authorise('core.create', 'com_jed.extension.' . $item->id);
+				$canEdit = $user->authorise('core.edit', 'com_jed.extension.' . $item->id);
+				$canCheckin = $user->authorise('core.manage',
+						'com_checkin') || $item->checked_out === $userId || $item->checked_out === 0;
+				$canEditOwn = $user->authorise('core.edit.own',
+						'com_jed.extension.' . $item->id) && $item->created_by === $userId;
+				$canChange = $user->authorise('core.edit.state', 'com_jed.extension.' . $item->id) && $canCheckin;
+				?>
+                <tr class="row<?php echo $i % 2; ?>">
+                    <td>
+						<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
+                    </td>
+                    <td class="center" width="50">
+                        <div class="btn-group">
+							<?php echo HTMLHelper::_('jgrid.published', $item->state, $i, 'extensions.', $canChange,
+								'cb', $item->publish_up, $item->publish_down); ?>
+							<?php
+							// Create dropdown items
+							$action = ($item->state === 1) ? 'unpublish' : 'publish';
+							HTMLHelper::_('actionsdropdown.' . $action, 'cb' . $i, 'extensions');
+
+							// Render dropdown list
+							echo HTMLHelper::_('actionsdropdown.render', $this->escape($item->name));
+							?>
+                        </div>
+                    </td>
+                    <td>
+						<?php echo $item->name; ?>
+                    </td>
+                    <td>
+						<?php echo $item->category; ?>
+                    </td>
+                    <td>
+						<?php echo $item->approved; ?>
+                    </td>
+                    <td>
+						<?php echo $item->developer; ?>
+                    </td>
+                    <td>
+						<?php echo $item->type; ?>
+                    </td>
+                    <td>
+						<?php echo $item->no_of_reviews; ?>
+                    </td>
+
+                    <td>
+						<?php echo $item->id; ?>
+                    </td>
+                </tr>
+			<?php endforeach; ?>
+            </tbody>
+        </table>
+        <input type="hidden" name="task" value=""/>
+        <input type="hidden" name="boxchecked" value="0"/>
+		<?php echo HTMLHelper::_('form.token'); ?>
+    </div>
 </form>
--- public_html/administrator/components/com_jed/views/extensions/view.html.php
+++ public_html/administrator/components/com_jed/views/extensions/view.html.php
@@ -8,80 +8,83 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\MVC\View\HtmlView;
-use Joomla\CMS\Language\Text;
+use Joomla\CMS\Form\Form;
 use Joomla\CMS\Helper\ContentHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView;
+use Joomla\CMS\Object\CMSObject;
+use Joomla\CMS\Pagination\Pagination;
+use Joomla\CMS\Toolbar\ToolbarHelper;
 
 /**
  * View for JED Extensions.
  *
- * @package   Joomla.JED
- * @since     4.0.0
+ * @package  JED
+ * @since    4.0.0
  */
 class JedViewExtensions extends HtmlView
 {
 	/**
-	 * An array of items
-	 *
-	 * @var     array
-	 * @since   4.0.0
-	 */
-	protected $items;
-
-	/**
-	 * The pagination object
-	 *
-	 * @var     JPagination
-	 * @since   4.0.0
-	 */
-	protected $pagination;
-
-	/**
-	 * The model state
-	 *
-	 * @var     object
-	 * @since   4.0.0
-	 */
-	protected $state;
-
-	/**
 	 * Form object for search filters
 	 *
-	 * @var     JForm
+	 * @var     Form
 	 * @since   4.0.0
 	 */
 	public $filterForm;
-
 	/**
 	 * The active search filters
 	 *
 	 * @var     array
 	 * @since   4.0.0
 	 */
-	public $activeFilters;
+	public $activeFilters = [];
+	/**
+	 * An array of items
+	 *
+	 * @var     array
+	 * @since   4.0.0
+	 */
+	protected $items = [];
+	/**
+	 * The pagination object
+	 *
+	 * @var     Pagination
+	 * @since   4.0.0
+	 */
+	protected $pagination;
+	/**
+	 * The model state
+	 *
+	 * @var     CMSObject
+	 * @since   4.0.0
+	 */
+	protected $state;
 
 	/**
 	 * Display method of extensions view
 	 *
 	 * @param   string  $tpl  The template name
 	 *
-	 * @return string
+	 * @return  string
 	 *
-	 * @since  4.0.0
-	 * @throws Exception
+	 * @since   4.0.0
+	 *
+	 * @throws  Exception
 	 */
 	public function display($tpl = null)
 	{
-		$this->state         = $this->get('State');
-		$this->items         = $this->get('Items');
-		$this->pagination    = $this->get('Pagination');
-		$this->filterForm    = $this->get('FilterForm');
-		$this->activeFilters = $this->get('ActiveFilters');
+		/** @var JedModelExtensions $model */
+		$model               = $this->getModel();
+		$this->state         = $model->getState();
+		$this->items         = $model->getItems();
+		$this->pagination    = $model->getPagination();
+		$this->filterForm    = $model->getFilterForm();
+		$this->activeFilters = $model->getActiveFilters();
 
 		// Check for errors.
-		if (count($errors = $this->get('Errors')))
+		if (count($errors = $model->getErrors()))
 		{
-			throw new Exception(implode("\n", $errors), 500);
+			throw new RuntimeException(implode("\n", $errors), 500);
 		}
 
 		// Add the toolbar
@@ -96,52 +99,31 @@ class JedViewExtensions extends HtmlView
 	 * @return void
 	 *
 	 * @since  4.0.0
+	 *
 	 * @throws Exception
 	 */
-	protected function addToolBar()
+	protected function addToolBar(): void
 	{
-		$canDo = ContentHelper::getActions('com_jed', 'extension', $this->state->get('filter.published'));
+		$canDo = ContentHelper::getActions('com_jed', 'extension');
 
-		JToolBarHelper::title(Text::_('COM_JED_TITLE_EXTENSIONS'), 'plugin.png');
+		ToolBarHelper::title(Text::_('COM_JED_TITLE_EXTENSIONS'), 'plugin.png');
 
 		if ($canDo->get('core.create'))
 		{
-			JToolbarHelper::addNew('extension.add');
+			ToolbarHelper::addNew('extension.add');
 		}
 
-		if (($canDo->get('core.edit')) || ($canDo->get('core.edit.own')))
+		if ($canDo->get('core.edit') || $canDo->get('core.edit.own'))
 		{
-			JToolbarHelper::editList('extension.edit');
+			ToolbarHelper::editList('extension.edit');
 		}
 
 		if ($canDo->get('core.edit.state'))
 		{
-			JToolbarHelper::publish('extensions.publish', 'JTOOLBAR_PUBLISH', true);
-			JToolbarHelper::unpublish('extensions.unpublish', 'JTOOLBAR_UNPUBLISH', true);
+			ToolbarHelper::publish('extensions.publish', 'JTOOLBAR_PUBLISH', true);
+			ToolbarHelper::unpublish('extensions.unpublish', 'JTOOLBAR_UNPUBLISH', true);
 		}
 
 		JToolBarHelper::cancel('extensions.cancel', 'JTOOLBAR_CLOSE');
-		JToolBarHelper::spacer();
-	}
-
-	/**
-	 * Returns an array of fields the table can be sorted by
-	 *
-	 * @return  array  Array containing the field name to sort by as the key and display text as value
-	 *
-	 * @since   4.0.0
-	 */
-	protected function getSortFields()
-	{
-		return array(
-			't.title'       => Text::_('COM_JED_EXTENSIONS_TITLE'),
-			't.category'    => Text::_('COM_JED_EXTENSIONS_CATEGORY'),
-			't.published'   => Text::_('COM_JED_EXTENSIONS_PUBLISHED'),
-			't.approved'    => Text::_('COM_JED_EXTENSIONS_APPROVED'),
-			't.developer'   => Text::_('COM_JED_EXTENSIONS_DEVELOPER'),
-			't.type'        => Text::_('COM_JED_EXTENSIONS_TYPE'),
-			't.reviewCount' => Text::_('COM_JED_EXTENSIONS_REVIEWCOUNT'),
-			't.id'          => Text::_('JGRID_HEADING_ID')
-		);
 	}
 }
