a0d75b092a7e1b35c9a9e51472c6def31f30ad6b
Roland Dalmulder <contact@rolandd.com>
2020-07-18 20:38:18 +0200

    Set the correct year #15
    Signed-off-by: Roland Dalmulder <contact@rolandd.com>

--- public_html/administrator/components/com_jed/views/email/tmpl/edit.php
+++ public_html/administrator/components/com_jed/views/email/tmpl/edit.php
@@ -2,14 +2,14 @@
 /**
  * @package    JED
  *
- * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+defined('_JEXEC') or die;
+
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-
-defined('_JEXEC') or die;
 
 /** @var JedViewEmail $this */
 
